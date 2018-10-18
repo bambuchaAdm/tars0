001686e8a317e4fb0dd182140d9f6bc1799e4308
Andy Dwelly <andydwelly@gmail.com>
2018-10-18 16:13:26 +0100

    Added proper fail behaviour.

--- src/main/scala/FutureBool.scala
+++ src/main/scala/FutureBool.scala
@@ -1,17 +1,18 @@
 import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger}
 
+import scala.collection.mutable
 import scala.concurrent.Future.successful
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success, Try}
 
 object FutureBool extends App {
   /** Asynchronously and non-blockingly returns a `Future` that will hold the optional result
-    *  of the first `Future` with a result that matches the predicate.
+    * of the first `Future` with a result that matches the predicate.
     *
-    * @tparam T        the type of the value in the future
-    * @param futures   the `TraversableOnce` of Futures to search
-    * @param p         the predicate which indicates if it's a match
-    * @return          the `Future` holding the optional result of the search
+    * @tparam T the type of the value in the future
+    * @param futures the `TraversableOnce` of Futures to search
+    * @param p       the predicate which indicates if it's a match
+    * @return the `Future` holding the optional result of the search
     */
   def find[T](futures: TraversableOnce[Future[T]])(p: T => Boolean)(implicit executor: ExecutionContext): Future[Option[T]] = {
     val futuresBuffer = futures.toBuffer
@@ -36,9 +37,15 @@ object FutureBool extends App {
     }
   }
 
-  def ffind[T](futures: TraversableOnce[Future[T]])(p: T => Boolean)(implicit executor: ExecutionContext) = {
-    val futuresBuffer = futures.toBuffer
-    if (futuresBuffer.isEmpty) (successful[Option[T]](None), false)
+  def hasFailures[T](futuresBuffer: mutable.Seq[Future[T]]): Boolean = futuresBuffer.exists(
+    _.value match {
+      case Some(Success(x)) => false
+      case Some(Failure(e)) => true
+      case None => false
+    })
+
+  def ffind[T](futuresBuffer: mutable.Seq[Future[T]])(p: T => Boolean)(implicit executor: ExecutionContext) = {
+    if (futuresBuffer.isEmpty) successful[Option[T]](None)
     else {
       val result = Promise[Option[T]]()
       val ref = new AtomicInteger(futuresBuffer.size)
@@ -55,29 +62,29 @@ object FutureBool extends App {
 
       futuresBuffer.foreach(_ onComplete search)
 
-      val foundFails = futuresBuffer.exists(_.value match {
-        case Some(Success(x)) => false
-        case Some(Failure(e)) => true
-        case None => false
-      })
-
-      (result.future, foundFails)
+      result.future
     }
   }
 
 
   def all(futures: Future[Boolean]*)(implicit ec: ExecutionContext) = {
-    find(futures) { !_ } map {_.isEmpty}
-//    val result: (Future[Option[Boolean]], Boolean) = ffind(futures) {!_ }
-//    result._1 flatMap {
-//      case Some(f) => Future.successful(false)
-//      case None =>
-//        if (result._2) Future.failed(new RuntimeException("Fail detected in all when all others were true")) else Future.successful(true)
-//    }
+    //find(futures) {!_} map {_.isEmpty}
+    val buffer = futures.toBuffer
+    val result: Future[Option[Boolean]] = ffind(buffer) {!_}
+    result flatMap {
+      case Some(f) => Future.successful(false)
+      case None => if (hasFailures(buffer)) Future.failed(new RuntimeException("Fail detected in all when all others were true")) else Future.successful(true)
+    }
   }
 
   def any(futures: Future[Boolean]*)(implicit ec: ExecutionContext) = {
-    find(futures){ b => b} map {_.isDefined}
+    //find(futures) {identity} map {_.isDefined}
+    val buffer = futures.toBuffer
+    val result: Future[Option[Boolean]] = ffind(buffer) {identity}
+    result flatMap {
+      case Some(f) => Future.successful(true)
+      case None => if (hasFailures(buffer)) Future.failed(new RuntimeException("Fail detected in any when all others were false")) else Future.successful(false)
+    }
   }
 
 }
--- src/test/scala/FutureBoolSpec.scala
+++ src/test/scala/FutureBoolSpec.scala
@@ -14,6 +14,37 @@ class FutureBoolSpec extends FunSuite with ScalaFutures {
     (result, (end - start) / 1e9)
   }
 
+  //Found Fails
+  test("A mutable.Seq of Future.successful(true) returns false because it has no failures") {
+    val buffer = Seq(Future.successful(true), Future.successful(true), Future.successful(true)).toBuffer
+    val result = FutureBool.hasFailures(buffer)
+    assert(!result)
+  }
+
+  test("A mutable.Seq of Future.successful(false) returns false because it has no failures") {
+    val buffer = Seq(Future.successful(false), Future.successful(false), Future.successful(false)).toBuffer
+    val result = FutureBool.hasFailures(buffer)
+    assert(!result)
+  }
+
+  test("A mutable.Seq of a mix of Future.successful(true) and Future.successful(false) returns false because it has no failures") {
+    val buffer = Seq(Future.successful(false), Future.successful(false), Future.successful(false)).toBuffer
+    val result = FutureBool.hasFailures(buffer)
+    assert(!result)
+  }
+
+  test("An mutable.Seq returns false because it has no failures") {
+    val buffer = Seq[Future[Boolean]]().toBuffer
+    val result = FutureBool.hasFailures(buffer)
+    assert(!result)
+  }
+
+  test("A mutable Seq with Future.sucessful true, falses, and fails returns a true because it fails in it.") {
+    val buffer = Seq(Future.successful(false), Future.successful(false), Future.failed(new RuntimeException)).toBuffer
+    val result = FutureBool.hasFailures(buffer)
+    assert(result)
+  }
+
   //All
 
   test("all returns Future.successful(true) when all arguments are Future.successful(true)") {
@@ -32,7 +63,6 @@ class FutureBoolSpec extends FunSuite with ScalaFutures {
   }
 
   test("Failed futures are cause a fail when calculating a true result using all") {
-    pending
     val result = intercept[RuntimeException]{
       Await.result(FutureBool.all(Future.failed(new RuntimeException("Failed")), Future.successful(true), Future.successful(true)), 5 seconds)
     }
@@ -87,9 +117,11 @@ class FutureBoolSpec extends FunSuite with ScalaFutures {
     assert(result)
   }
 
-  test("Failed futures are ignored when calculating a false result using any") {
-    val result = Await.result(FutureBool.any(Future.failed(new RuntimeException("Failed")), Future.successful(false), Future.successful(false)), 5 seconds)
-    assert(!result)
+  test("Failed futures are cause a fail when calculating a false result using any") {
+    val result = intercept[RuntimeException]{
+      Await.result(FutureBool.any(Future.failed(new RuntimeException("Failed")), Future.successful(false), Future.successful(false)), 5 seconds)
+    }
+    assert(result.getMessage == "Fail detected in any when all others were false")
   }
 
   test ("any returns true fast if there are trues and falses, but falses are delayed") {
